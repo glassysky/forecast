6066bf9a34d8160dc1084bf201ee357ec80e0478
glassysky <873661157@qq.com>
2017-03-27 10:44:08 +0800

    delete escapes

--- src/utils/LocalStorage.js
+++ src/utils/LocalStorage.js
@@ -2,15 +2,15 @@ if (!window.localStorage) {
   window.localStorage = {
     getItem(sKey) {
       if (!sKey || !this.hasOwnProperty(sKey)) { return null; }
-      return unescape(document.cookie.replace(new RegExp(`(?:^|.*;\\s*)${escape(sKey).replace(/[\-\.\+\*]/g, '\\$&')}\\s*\\=\\s*((?:[^;](?!;))*[^;]?).*`), '$1'));
+      return unescape(document.cookie.replace(new RegExp(`(?:^|.*;\\s*)${escape(sKey).replace(/[-.+*]/g, '\\$&')}\\s*\\=\\s*((?:[^;](?!;))*[^;]?).*`), '$1'));
     },
     key(nKeyId) {
-      return unescape(document.cookie.replace(/\s*\=(?:.(?!;))*$/, '').split(/\s*\=(?:[^;](?!;))*[^;]?;\s*/)[nKeyId]);
+      return unescape(document.cookie.replace(/\s*=(?:.(?!;))*$/, '').split(/\s*=(?:[^;](?!;))*[^;]?;\s*/)[nKeyId]);
     },
     setItem(sKey, sValue) {
       if (!sKey) { return; }
       document.cookie = `${escape(sKey)}=${escape(sValue)}; expires=Tue, 19 Jan 2038 03:14:07 GMT; path=/`;
-      this.length = document.cookie.match(/\=/g).length;
+      this.length = document.cookie.match(/=/g).length;
     },
     length: 0,
     removeItem(sKey) {
@@ -19,10 +19,10 @@ if (!window.localStorage) {
       this.length = this.length - 1;
     },
     hasOwnProperty(sKey) {
-      return (new RegExp(`(?:^|;\\s*)${escape(sKey).replace(/[\-\.\+\*]/g, '\\$&')}\\s*\\=`)).test(document.cookie);
+      return (new RegExp(`(?:^|;\\s*)${escape(sKey).replace(/[-.+*]/g, '\\$&')}\\s*\\=`)).test(document.cookie);
     },
   };
-  window.localStorage.length = (document.cookie.match(/\=/g) || window.localStorage).length;
+  window.localStorage.length = (document.cookie.match(/=/g) || window.localStorage).length;
 }
 
 const storage = window.localStorage;
